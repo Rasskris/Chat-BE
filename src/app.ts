cf7a441048d361806d15f98fa1e4583e524b1f6b
Rasskris <rassoshenko.kristina@yandex.ru>
2023-10-08 20:56:40 +0300

    feat: add server

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import express from 'express';
+import { createServer, Server } from 'http';
+
+import Socket from './services/socket';
+
+class App {
+  public app: express.Application;
+
+  public port = process.env.PORT;
+
+  public server: Server;
+
+  public socket: Socket;
+
+  constructor() {
+    this.app = express();
+    this.server = createServer(this.app);
+    this.socket = new Socket(this.server);
+
+    this.initializeSocketConnection();
+  }
+
+  public listen() {
+    this.server.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`);
+    });
+  }
+
+  public initializeSocketConnection() {
+    this.socket
+      .init()
+      .start();
+  }
+}
+
+export default App;
